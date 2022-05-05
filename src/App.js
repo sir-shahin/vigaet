3e466acb12996dc3fc32836ff7274a3cca4da44e
unknown <shahin.n14001995@gmail.com>
2022-05-05 20:29:06 +0430

    First push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Onboard from "./pages/Onboard"
+import Dashboard from "./pages/Dashboard"
+import Check from "./pages/Check"
+import Exam from "./pages/Exam"
+import Settings from "./pages/Settings"
+import Header from './components/Header'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+            <Header />
+        </div>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/onboard" element={<Onboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/check" element={<Check />} />
+            <Route path="/exam" element={<Exam />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
